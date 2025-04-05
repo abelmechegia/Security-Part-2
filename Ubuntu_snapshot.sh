d352da21f0e43146d17c3578d31f6b8ba98ad7c6
abelmechegia <abelmechegia@gmail.com>
2025-04-05 12:35:04 -0400

    Add files via upload

--- Ubuntu_snapshot.sh
+++ Ubuntu_snapshot.sh
@@ -5,8 +5,8 @@ CURRENT_FILE="/home/abel/current_users.txt"
 UPDATE_FILE="/home/abel/update_users.log"
 
 # Take a snapshot of existing users and compare with the previous snapshot
-awk -F: '{print $1}' /etc/passwd | tee /home/abel/current_users.new | \
-diff "$CURRENT_FILE" - >> "$UPDATE_FILE"
+awk -F: '{print $1}' /etc/passwd > /home/abel/current_users.new
+diff "$CURRENT_FILE"  /home/abel/current_users.new >> "$UPDATE_FILE"
 
 # Update the latest snapshot
 mv /home/abel/current_users.new "$CURRENT_FILE"
